416315a6ffde6fe4d20944b831eaae2213067b06
Mrinal Kumar Jha <mrjha@deloitte.com>
2025-01-30 18:08:48 +0530

    CQRS abstraction created inside building block

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        :ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            // business logic to create product..
+            throw new NotImplementedException();
+        }
+    }
+}
+
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to container
+
+var app = builder.Build();
+
+// Configure HTTP request pipeline
+
+
+
+app.Run();
