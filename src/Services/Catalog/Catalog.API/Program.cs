ba0f478e8a114a19f9dbce92b7d1a26158531e76
Mrinal Kumar Jha <mrjha@deloitte.com>
2025-01-31 11:20:31 +0530

    Added swagger in api project

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,12 +1,35 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to container
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Carter API",
+        Description = "An example API built with Carter",
+        Version = "v1"
+    });
+});
 builder.Services.AddCarter();
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+
 
 var app = builder.Build();
 
 // Configure HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "Carter API V1");
+            options.RoutePrefix = string.Empty;
+        }
+        );
+}
 
 app.MapCarter();
 
