acf528a78cd8ce5859e578eb5dfbb615d2df6e03
Mrinal Kumar Jha <mrjha@deloitte.com>
2025-02-04 17:58:51 +0530

    GetProductByCategoryEndpoint and handler added

--- src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -1,6 +1,20 @@
-﻿namespace Catalog.API.Products.GetProductByCategory
+﻿using Catalog.API.Products.GetProducts;
+using Marten.Linq.QueryHandlers;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Products.GetProductByCategory
 {
-    public class GetProductByCategoryHandler
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
     {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler called with {@Query}", query);
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync(cancellationToken);
+            return new GetProductByCategoryResult(products);
+        }
     }
 }
