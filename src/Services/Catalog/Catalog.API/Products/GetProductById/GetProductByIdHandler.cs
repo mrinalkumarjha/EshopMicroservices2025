24f7f93b461efbda323f13698e780ee035d399e8
Mrinal Jha <mrinalkumarjha@ymail.com>
2025-02-04 12:09:16 +0530

    added GetProductByIdEndpoint and handler

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Products.GetProducts;
+using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQueryHandler called with {@Query}", query);
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+            if (product is null) {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByIdResult(product);
+        }
+    }
+}
